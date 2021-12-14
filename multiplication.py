ff0fd3b9740922e459aa7c0440667ce8df306798
Brandon Arreguin <arreguin.tech@gmail.com>
2021-12-13 20:26:33 -0600

    Update

--- multiplication.py
+++ multiplication.py
@@ -1,7 +1,10 @@
 def run():
-    number = int(input('Multiplication table: '))
     for i in range(1, 11):
-        print(i * number)
+        print(' ')
+        print(f'Tabla del {i}')
+        for j in range(1, 11):
+            print(f'{i} x {j} = {i*j}')
+
 
 if __name__ == '__main__':
     run()
